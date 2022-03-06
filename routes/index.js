cd3401caf90b43ad05fc70f97254146e0e651f52
Rahul Goyal <rhgoyal01@gmail.com>
2022-03-06 23:47:26 +0530

    Remove / endpoint

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ const controller = require('../controller/user');
 
 const app = express();
 
-app.get('/',(req,res)=>{res.send("Home")})
 app.get('/health', controller.getHealth)
 app.get('/profile', controller.getProfile)
 app.post('/verification', controller.verification)
