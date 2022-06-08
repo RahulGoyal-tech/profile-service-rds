f0cf0e49bdedc384c8cd5ccad22d0a6f6175d1e3
Rahul Goyal <rhgoyal01@gmail.com>
2022-06-09 02:47:17 +0530

    Updated Chaincode and yoe

--- credentials/credentials.js
+++ credentials/credentials.js
@@ -3,7 +3,7 @@ const DATA = {
     last_name: "Goyal",
     email: "rhgoyal01@gmail.com",
     phone: "+91-9540717412",
-    yoe: 1,
+    yoe: 0,
     company: "Bharati Vidyapeeth college of engineering",
     designation: "Student",
     github_id: "RahulGoyal-tech",
@@ -11,7 +11,7 @@ const DATA = {
     twitter_id: "Rahul1Goyal",
     instagram_id: "",
     website: "",
-    chain_code: "UrjezZ5eZYr4uBJiJWIP",
+    chain_code: "Ql0r67V8n6YOgiA0PhAQ",
 };
 
 module.exports = DATA;
