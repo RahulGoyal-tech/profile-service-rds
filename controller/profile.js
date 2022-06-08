9cec420d15c2b25436038a4a14fa4492cdff64a8
Rahul Goyal <rhgoyal01@gmail.com>
2022-06-09 02:59:54 +0530

    Taking hash from authorization

--- controller/profile.js
+++ controller/profile.js
@@ -6,7 +6,7 @@ const getHealth = (_,res) => {
 }
 const getProfile = (req,res) => {
     try{
-        const hash = req.headers.bearertoken;
+        const hash = req.headers.authorization;
         if (bcrypt.compareSync(data.chain_code, hash)){}
         else {return res.status(403).send("Invalid Hash")}
         res.send(JSON.stringify({
