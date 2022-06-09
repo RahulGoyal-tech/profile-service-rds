66bb4807ae5b6cf3ec7f3a1293c402ae3f807c85
Rahul Goyal <rhgoyal01@gmail.com>
2022-06-09 19:52:56 +0530

    Re Added Credentials file

---
+++ credentials/credentials.js
@@ -0,0 +1,17 @@
+const DATA = {
+    first_name: "Rahul",
+    last_name: "Goyal",
+    email: "rhgoyal01@gmail.com",
+    phone: "+91-9540717412",
+    yoe: 0,
+    company: "Bharati Vidyapeeth college of engineering",
+    designation: "Student",
+    github_id: "RahulGoyal-tech",
+    linkedin_id: "rahul-goyal-69a9801b1",
+    twitter_id: "Rahul1Goyal",
+    instagram_id: "",
+    website: "",
+    chain_code: "Ql0r67V8n6YOgiA0PhAQ",
+};
+
+module.exports = DATA;
